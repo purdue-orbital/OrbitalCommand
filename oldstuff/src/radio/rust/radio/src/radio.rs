525d718f18c597072ba0a27ca69d22dabbb0b6a1
Jack Hogan <jackhogan11@gmail.com>
2023-03-22 18:03:20 -0400

    Reformatted everything

---
+++ oldstuff/src/radio/rust/radio/src/radio.rs
@@ -0,0 +1,40 @@
+use std::any::type_name;
+use soapysdr::{Device, Error};
+
+// Radio Values
+
+#[derive(Clone)]
+pub struct Radio {
+    device: Option<Device>,
+}
+
+impl Radio
+{
+    pub fn new() -> Result<Radio, Error>
+    {
+        // Initialize values of a radio
+        let mut new_radio = Radio { device: None };
+
+        // Set that we are looking for lime devices
+        let mut args = soapysdr::Args::new();
+        args.set("device", "lime");
+
+        //soapysdr::Device::
+        for x in soapysdr::enumerate("device=lime").unwrap()
+        {
+            println!("{}",x);
+        }
+
+        // get list of radios
+        let err = soapysdr::Device::new(args)?;
+
+        // if we find a radio and connect to it, stop looping
+        new_radio.device = Some(err);
+        Ok(new_radio)
+    }
+
+    pub fn get_radio(&self) -> &Device
+    {
+        self.device.as_ref().expect("Get Radio Instance")
+    }
+}
