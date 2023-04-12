ff1b254de2d805139571f0d6a1aca173998f6130
Jack Hogan <jackhogan11@gmail.com>
2023-04-12 18:49:33 -0400

    Added build script, updated README

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ## OrbitalCommand
+Purdue Orbital's Avionics and Ground Control Library, now with 100% more Rust!
 
 ### Building
 Build the code:
 ```
-./build.sh <ground|launch> [load|savepi]
+./build.sh <ground|launch> [load]
 ```
 
-First argument determines which station to build, second optional argument determines whether to load into Docker, save the
-image to a Pi-compatible TAR file, or do nothing if the argument is omitted.
+First argument determines which station to build, second optional argument determines whether to load into Docker. 
+If no second argument is given, a tarball file of the container is left in the root directory.
---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: argument 'ground' or 'launch' required"
+  exit 1
+elif [ "$1" != "ground" ] && [ "$1" != "launch" ]; then
+  echo "Error: first argument must be 'ground' or 'launch'"
+  exit 1
+fi
+
+if [ -n "$2" ]; then
+  if [ "$2" != "load" ]; then
+    echo "Error: second argument must be 'load'"
+    exit 1
+  fi
+fi
+
+if [ "$1" = "ground" ]; then
+  echo "Ground Station Build"
+  # Run ground command here
+  docker buildx build --output type=docker,dest=./image.tar -t "ground:latest" .
+elif [ "$1" = "launch" ]; then
+  echo "Launch Station Build"
+  # Run launch command here
+  docker buildx build --build-arg TARGET_CRATE=launch --output type=docker,dest=./image.tar -t "launch:latest" .
+fi
+
+if [ "$2" = "load" ]; then
+  echo "Loading Image..."
+  # Run load command here
+  docker load --input image.tar
+fi
