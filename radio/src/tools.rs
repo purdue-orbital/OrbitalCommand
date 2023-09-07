310553914ddf570442ec128b6b92048a9a7e044d
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2023-09-07 19:05:39 -0400

    Removed uses of unwraps

--- radio/src/tools.rs
+++ radio/src/tools.rs
@@ -2,25 +2,6 @@ use std::str::Chars;
 
 use num_complex::Complex;
 
-/// Subtract values with the value to their left. This will remove 1 element from the array size
-///
-/// # Arguments
-/// * `arr` - Array Of Values To Preform Operations
-///
-pub fn subtract_left_adjacent(arr: Vec<f32>) -> Vec<f32>
-{
-    // Left shift values
-    let mut out = Vec::new();
-
-    for x in 1..arr.len()
-    {
-        out.push(arr.clone().get(x - 1).unwrap() - arr.clone().get(x).unwrap())
-    }
-
-    out
-}
-
-
 /// Find the moving average of an array of numbers
 ///
 /// # Arguments
@@ -62,23 +43,6 @@ pub fn average_complex(arr: Vec<Complex<f32>>) -> f32 {
     sum / arr.len() as f32
 }
 
-/// Normalize values of an array of numbers
-///
-/// # Arguments
-/// * `arr` - Array Of Values To Preform Operations
-/// * `size` - Number of values to average together to form average
-///
-pub fn normalize(arr: Vec<f32>) -> Vec<f32>
-{
-    // Get max
-    let max = *arr.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-
-    // Get min
-    let min = *arr.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).unwrap();
-
-    // Normalize values and return
-    arr.iter().map(|&x| (x - min) / (max - min)).collect()
-}
 
 /// Although the format! macro does this for us, we sometimes want to dynamically set the fixed
 /// length of the formatted binary
