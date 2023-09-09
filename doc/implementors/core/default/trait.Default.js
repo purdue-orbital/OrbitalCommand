a02d672833aab2261c6c11c9b6447d584f5f20c8
Nicholas Ball <89214070+Nicholas-Ball@users.noreply.github.com>
2023-09-09 16:03:24 -0400

    Fixed folder name

--- doc/implementors/core/default/trait.Default.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"net":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"net/layer_3/ipv4/struct.IPV4.html\" title=\"struct net::layer_3::ipv4::IPV4\">IPV4</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/marker/trait.StructuralPartialEq.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"net":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"net/interface/enum.Interface.html\" title=\"enum net::interface::Interface\">Interface</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
