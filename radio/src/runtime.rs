7d4b97f8117b376dd167fd7d15903c16e46d41a2
Kerbidiah <alexjanninck@gmail.com>
2023-10-19 15:36:19 -0400

    converting mod and demod to pipeline (inprogress)

--- radio/src/runtime.rs
+++ radio/src/runtime.rs
@@ -2,7 +2,8 @@ use std::sync::{Arc, RwLock};
 use std::thread::{sleep, spawn};
 use std::time::Duration;
 
-use flume::Receiver;
+use flume::{Receiver, Sender};
+use bytes::Bytes;
 use rustdsp::Modulators;
 
 use crate::radio_settings::RadioSetting;
@@ -12,12 +13,15 @@ use crate::radios::bladerf::stream::BladeRFTxStream;
 pub struct Runtime {
     radio_setting: RadioSetting,
     tx_stream: BladeRFTxStream,
-    rx_channel: Receiver<Vec<u8>>,
+    /// the flume channel we use to send data onto the next step of the pipeline
+    tx_bits: Sender<u8>,
+    /// the flume channel we use to get bytes to be sent
+    rx_bytes: Receiver<Bytes>,
     modulator: Modulators,
 }
 
 impl Runtime {
-    pub fn new(settings: RadioSetting) -> anyhow::Result<Runtime> {
+    pub fn new(settings: RadioSetting, bytes_channel: Receiver<Bytes>) -> anyhow::Result<(Runtime, Receiver<u8>)> {
 
         // Connect to radio
         let radio = Arc::new(RwLock::new(BladeRF::default()));
@@ -30,11 +34,46 @@ impl Runtime {
         tx_stream.set_sample_rate(settings.sample_rate as u64)?;
 
         // create rx channel
-        let (tx, rx) = flume::unbounded();
+        let (tx_bits, rx) = flume::unbounded();
 
         let samples_per_a_symbol = (settings.sample_rate / settings.baud_rate) as usize;
 
         // Create RX loop
+
+        Ok((
+            Runtime {
+                radio_setting: settings,
+                tx_stream,
+                tx_bits,
+                rx_bytes: bytes_channel,
+                modulator: Modulators::new(samples_per_a_symbol, settings.sample_rate),
+            },
+            rx
+        ))
+    }
+
+    // pub fn tx(&self, bin: &[u8]) {
+    //     // modulate
+    //     let modulated = self.modulator.bpsk(bin);
+
+    //     // send
+    //     self.tx_stream.tx(modulated.as_slice());
+    // }
+
+    // pub fn rx(&self) -> Vec<u8> {
+    //     let mut out = self.rx_channel.recv().unwrap();
+
+    //     while out.is_empty() {
+    //         out = self.rx_channel.recv().unwrap();
+
+    //         sleep(Duration::from_millis(1));
+    //     }
+
+    //     out
+    // }
+
+    pub fn run(self) {
+        // recieving thread
         spawn(move || {
             let d = rustdsp::Demodulators::new(samples_per_a_symbol, settings.baud_rate);
 
@@ -46,21 +85,6 @@ impl Runtime {
             rx_stream.set_sample_rate(settings.sample_rate as u64).unwrap();
 
             loop {
-                // // sample
-                // let mtu = rx_stream.rx(samples_per_a_symbol);
-                //
-                // // demod
-                // let bit = d.bpsk(mtu)[0];
-                //
-                // bin <<= 1;
-                // bin |= bit;
-                // bin_counter += 1;
-                //
-                // if bin_counter == 8{
-                //
-                // }
-
-                // let mut bin_counter: u8 = 0;
                 let mut bin: u8 = 0;
 
                 for _ in 0..8 {
@@ -71,36 +95,15 @@ impl Runtime {
                     bin |= bit;
                 }
 
-                tx.send(bin).unwrap();
+                self.tx_channel.send(bin).unwrap();
             }
         });
 
-
-        Ok(Runtime {
-            radio_setting: settings,
-            tx_stream,
-            rx_channel: rx,
-            modulator: Modulators::new(samples_per_a_symbol, settings.sample_rate),
-        })
-    }
-
-    pub fn tx(&self, bin: &[u8]) {
-        // modulate
-        let modulated = self.modulator.bpsk(bin);
-
-        // send
-        self.tx_stream.tx(modulated.as_slice());
-    }
-
-    pub fn rx(&self) -> Vec<u8> {
-        let mut out = self.rx_channel.recv().unwrap();
-
-        while out.is_empty() {
-            out = self.rx_channel.recv().unwrap();
-
-            sleep(Duration::from_millis(1));
-        }
-
-        out
+        spawn(move || {
+            while let Some(bin) = self.rx_bytes.recv() {
+                (&self).modulator.bpsk(bin.to_vec());
+                (&self).
+            }
+        });
     }
 }
